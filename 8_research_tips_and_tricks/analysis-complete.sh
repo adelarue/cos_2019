a66608687bbacef5fa9f6cc330a617b61eb4a8fd
adelarue <adelarue@mit.edu>
2019-01-30 14:58:46 -0500

    More consistency

--- 8_research_tips_and_tricks/analysis-complete.sh
+++ 8_research_tips_and_tricks/analysis-complete.sh
@@ -9,4 +9,4 @@
 #SBATCH --output=logs/experiment_\%a.log
 module load sloan/julia/1.0.0
 module load sloan/python/modules/2.7
-srun julia analysis.jl $SLURM_ARRAY_TASK_ID
+srun julia analysis-complete.jl $SLURM_ARRAY_TASK_ID
