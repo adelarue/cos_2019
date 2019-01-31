b9b03d96c17d29bb8c3fbc67a6640197a240d2b0
adelarue <adelarue@mit.edu>
2019-01-31 10:06:51 -0500

    Added analysis.jl script

--- 8_research_tips_and_tricks/analysis.jl
+++ 8_research_tips_and_tricks/analysis.jl
@@ -7,19 +7,30 @@ using JLD2
 @sk_import ensemble:RandomForestClassifier
 
 # Import our training function which trains a random forest
-
+include("rf.jl")
 
 # Load the data for training and validation
-
+train = CSV.read("listings_clean_train.csv", header = false)
+val = CSV.read("listings_clean_val.csv", header = false)
 
 # Load the parameters dataframe
-
+@load "params.jld2" params
 
 # Get the experiment ID from the arguments that were passed to julia
-
+experiment_id = ifelse(length(ARGS) > 0, parse(Int, ARGS[1]), 1)
 
 # Set a random seed - always a good idea when using functions you didn't write
 Random.seed!(1776)
 
-# run the experiment for the desired experiment ID
 
+# run the experiment for the desired experiment ID
+for i = 1:nrow(params)
+	if params[i, :experiment_id] != experiment_id
+		continue
+	end
+	R2tr, R2va = trainrandomforest(train, val,
+	                               params[i, :max_depth],
+	                               params[i, :n_estimators],
+	                               params[i, :max_features])
+	@save "results/results-$experiment_id.jld2" R2tr R2va
+end
--- 8_research_tips_and_tricks/parameters.jl
+++ 8_research_tips_and_tricks/parameters.jl
@@ -11,20 +11,28 @@ numestimators = [20, 40, 80, 160]
 	where each row corresponds to the parameters for one experiment
 """
 function getparameters(depths, maxfeatures, numestimators)
-	df = DataFrame()
-	# TODO
-
-
-
-
+	df = DataFrame(experiment_id = [],
+	               max_depth = [],
+	               max_features = [],
+	               n_estimators = [])
+	experiment_id = 0
+	for depth in depths, mf in maxfeatures, nest in numestimators
+		experiment_id += 1
+		df = [df;
+				DataFrame(experiment_id = [experiment_id],
+	               max_depth = [depth],
+	               max_features = [mf],
+	               n_estimators = [nest])]
+	end
 	return df
 end
 
 # Let's get these parameters into the right format
 params = getparameters(depths, maxfeatures, numestimators)
 # and now let's save them to a JLD file
-
+@save "params.jld2" params
 
 # Let's make a folder to save the results (if necessary)
-
+try mkdir("results") catch e end
+try mkdir("logs") catch e end
 
