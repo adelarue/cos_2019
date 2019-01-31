0f4ab96a58bf70300465f9c83a931a7959955fcb
adelarue <adelarue@mit.edu>
2019-01-31 10:23:31 -0500

    Analysis.sh

--- 8_research_tips_and_tricks/analysis.sh
+++ 8_research_tips_and_tricks/analysis.sh
@@ -1,1 +1,12 @@
 #!/bin/sh
+#SBATCH -a 1-200
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1G
+#SBATCH -p sched_mit_sloan_batch
+#SBATCH --output=logs/experiment_\%a.log
+#SBATCH --mail-user=adelarue@mit.edu
+#SBATCH --mail-type=END,FAIL
+
+module load sloan/julia/1.0.0
+module load sloan/python/modules/2.7
+srun julia analysis.jl $SLURM_ARRAY_TASK_ID
